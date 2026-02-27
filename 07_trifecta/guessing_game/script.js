4497fc06173698dba6d95d9729819ce348e92378
thegreencrayon2 <thegreencrayon2@gmail.com>
2026-02-27 16:32:14 -0500

    Create a working JS that uses yes no button to confirm guesses and give clarifying hints to the program. It was a pain because I had to learn the hard way that it's not a good idea to have multiple yes listeners for a single button. It's not a good idea to nest listeners either.

---
+++ 07_trifecta/guessing_game/script.js
@@ -0,0 +1,61 @@
+// variables
+let start = 0;
+let end = 100;
+let guess = 50;
+let qType = "guess";
+
+// access
+const result = document.getElementById("result");
+const promptBox = document.getElementById("prompt");
+const yesBtn= document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+
+makeGuess();
+
+// yes button either confirms guess or says that guess needs to be bigger
+yesBtn.addEventListener("click", function() {
+    if (qType == "guess") {
+        result.textContent = `The number is ${guess}!`;
+        promptBox.textContent = "";
+        yesBtn.style.display = "none";
+        noBtn.style.display = "none";
+    } else if (qType == "clarify") {
+        guess = Math.floor((guess + end) / 2);
+        updateGuess();
+    }
+});
+
+
+// no button either asks for clarification or says that guess needs to be smaller
+noBtn.addEventListener("click", function() {
+    if (qType == "guess") {
+        askClarification();
+    } else if (qType == "clarify") {
+        guess = Math.floor((start + guess) / 2);
+        updateGuess();
+    }
+});
+
+
+function askClarification() {
+    promptBox.textContent = `Is the number greater than ${guess}?`
+    qType = "clarify";
+};
+
+function makeGuess() {
+    promptBox.textContent = `Is your number ${guess}?`;
+}
+
+function updateGuess() {
+    qType="guess";
+    // display updated guess
+    makeGuess();
+}
+
+/* CONFUSING TAKEAWAYS
+1. an access list is really helpful 
+2. leave a single no listener, and a single yes listener per button
+3. use a status variable to determine meaning for different yes/no presses
+4. if you decide to do multiple no/yes listeners, prepare for dupes
+*/
